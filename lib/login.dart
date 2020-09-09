2c9735fed4dad8146013ab7edc2f3c971885cc1d
mduduzi <zwanemduduzi9@gmail.com>
2020-09-09 10:35:07 +0200

    api login with token

--- lib/login.dart
+++ lib/login.dart
@@ -3,29 +3,25 @@ import 'homescrean.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 //import 'FadeAnimation.dart';
 
-
-class LoginBody extends StatefulWidget{
+class LoginBody extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _Login();
   }
-
 }
 
-class _Login extends State<LoginBody>{
-  final formKey=GlobalKey<FormState>();
-  TextEditingController emailController= new TextEditingController();
-  TextEditingController passController= new TextEditingController();
+class _Login extends State<LoginBody> {
+  final formKey = GlobalKey<FormState>();
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passController = new TextEditingController();
   var response;
 
   String message = '';
 
-  Future userLogin(String username,String password) async {
-
-  }
-
+  Future userLogin(String username, String password) async {}
 
   @override
   Widget build(BuildContext context) {
@@ -33,153 +29,147 @@ class _Login extends State<LoginBody>{
       backgroundColor: Colors.white,
       body: Container(
         decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage('images/backend.jpg'),
-                fit: BoxFit.fill
-            )
-        ),
+            image: DecorationImage(
+                image: AssetImage('images/backend.jpg'), fit: BoxFit.fill)),
         child: ListView(
           children: <Widget>[
             Container(
               height: 300,
               decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage('images/logo1.png'),
-                  fit: BoxFit.cover
-                )
-              ),
+                  image: DecorationImage(
+                      image: AssetImage('images/logo1.png'),
+                      fit: BoxFit.cover)),
             ),
             Form(
               key: formKey,
-             child: Padding(padding: EdgeInsets.all(10.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromRGBO(143, 148, 251, 3),
-                            blurRadius: 20.0,
-                            offset: Offset(0, 10),
-                          )]
-                    ),
-
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                            border: Border(bottom: BorderSide(color: Colors.grey[900]))
-                          ),
-
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Email",
-                              hintStyle: TextStyle(color: Colors.grey[400])
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromRGBO(143, 148, 251, 3),
+                              blurRadius: 20.0,
+                              offset: Offset(0, 10),
+                            )
+                          ]),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom:
+                                        BorderSide(color: Colors.grey[900]))),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Email",
+                                  hintStyle:
+                                      TextStyle(color: Colors.grey[400])),
+                              controller: emailController,
                             ),
-                            controller: emailController,
                           ),
-
-                        ),
-
-                        Container(
-                          padding: EdgeInsets.all(8.0),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Password",
-                                hintStyle: TextStyle(color: Colors.grey[400])
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Password",
+                                  hintStyle:
+                                      TextStyle(color: Colors.grey[400])),
+                              obscureText: true,
+                              controller: passController,
                             ),
-                            obscureText: true,
-                            controller: passController,
-                          ),
-                        )
-                      ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-
-                  SizedBox(height: 15,),
-                  Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient: LinearGradient(colors: [
-                        Color.fromRGBO(143, 148, 251, 1),
-                        Color.fromRGBO(143, 148, 251, 6),
-                      ])
+                    SizedBox(
+                      height: 15,
                     ),
-
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.lightBlue[900],
-                      child: Text('Login'),
-
-                      onPressed: ()async {
-                        if(formKey.currentState.validate())  {
-
-                          setState(() {
-                            //message = 'please wait';
-                          });
-
-                          Map<String, String> headers = {"content-type": "application/json",
-                            "Accept": "application/json"};
-                          final body = jsonEncode({
-
-                            'username': emailController.text,
-                            'password': passController.text,
-                          });
-                          response = await http.post(
-                              'https://app.idolconsulting.co.za/idols/users/login',
-
-                              headers: headers,
-
-                              body: body
-
-                          );
-
-                          var loginMessage;
-                          print(response.statusCode);
-                          if (response.statusCode == 200) {
-                            message = "login success";
-
-                            setState((){
-                              loginMessage=json.decode(response.body);
-                              //If the authentication is successful switch to the home page
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-                              // loginMessage['message'];
-                              //print(loginMessage.toString());
-                            }
-
-                            );
-                          }
-                          else{
-                            loginMessage=json.decode(response.body);
+                    Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(colors: [
+                            Color.fromRGBO(143, 148, 251, 1),
+                            Color.fromRGBO(143, 148, 251, 6),
+                          ])),
+                      child: RaisedButton(
+                        textColor: Colors.white,
+                        color: Colors.lightBlue[900],
+                        child: Text('Login'),
+                        onPressed: () async {
+                          if (formKey.currentState.validate()) {
                             setState(() {
-                              message = "login failed";
+                              //message = 'please wait';
                             });
-                            //Display the server message to the user if the login failed
-                            // return new Text( loginMessage['message'],style: TextStyle(fontSize: 50,color: Colors.green),);
-                            //print(response.body);
-                            Scaffold.of(context).showSnackBar(SnackBar(content: Text(loginMessage['message'],)));
+
+                            Map<String, String> headers = {
+                              "content-type": "application/json",
+                              "Accept": "application/json"
+                            };
+                            final body = jsonEncode({
+                              'username': emailController.text,
+                              'password': passController.text,
+                            });
+                            SharedPreferences sharedPreferences =
+                                await SharedPreferences.getInstance();
+                            response = await http.post(
+                                'https://app.idolconsulting.co.za/idols/users/login',
+                                headers: headers,
+                                body: body);
+
+                            var loginMessage;
+                            print(response.statusCode);
+                            if (response.statusCode == 200) {
+                              message = "login success";
+
+                              setState(() {
+                                loginMessage = json.decode(response.body);
+
+                                //If the authentication is successful switch to the home page
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Home()));
+                                // loginMessage['message'];
+                                //print(loginMessage.toString());
+                              });
+                              sharedPreferences.setString(
+                                  "token", loginMessage['token']);
+                            } else {
+                              loginMessage = json.decode(response.body);
+                              setState(() {
+                                message = "login failed";
+                              });
+                              //Display the server message to the user if the login failed
+                              // return new Text( loginMessage['message'],style: TextStyle(fontSize: 50,color: Colors.green),);
+                              //print(response.body);
+                              Scaffold.of(context).showSnackBar(SnackBar(
+                                  content: Text(
+                                loginMessage['message'],
+                              )));
+                            }
+                            print(message);
                           }
-                          print(message);
-                        }
-                      },
-
+                        },
+                      ),
                     ),
-
-                  ),
-                  Text(message),
-                ],
+                    Text(message),
+                  ],
+                ),
               ),
-            ),
             ),
           ],
         ),
       ),
     );
-
   }
 }
