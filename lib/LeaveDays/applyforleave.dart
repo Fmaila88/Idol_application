976ba638aed8081ad482f2ce8fe25285de0368f0
Freddy_cloud <lebogang54>
2020-09-30 14:55:30 +0200

    leave dayss

--- lib/LeaveDays/applyforleave.dart
+++ lib/LeaveDays/applyforleave.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Leave days',
+      title: 'Leave dayss',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.grey,
