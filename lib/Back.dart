3e7c84ffbb74b2612b700f25d7487dba609ad634
mduduzi <zwanemduduzi9@gmail.com>
2020-10-05 10:54:43 +0200

    timesheet

--- lib/Back.dart
+++ lib/Back.dart
@@ -1,11 +1,21 @@
 class Back {
-  String startDate;
-  String endDate;
+  String startTime;
+  String endTime;
   String comments;
+  String currentDate;
 
-  Back({this.startDate, this.endDate, this.comments});
+  Back({this.startTime, this.endTime, this.comments, this.currentDate});
 
-  String getStartDate() {
-    return this.startDate;
+  String getStartTime() {
+    //this.startDate = time;
+    return this.startTime;
+  }
+
+  String getendTime() {
+    return this.endTime;
+  }
+
+  String getcomments() {
+    return this.comments;
   }
 }
