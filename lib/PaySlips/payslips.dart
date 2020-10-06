aa0b03eecf67af82e64434e38118338c990288f8
DESKTOP-S8CEDOK\Dell <portian191@gmail.com>
2020-10-06 14:56:00 -0700

    payslip

--- lib/PaySlips/payslips.dart
+++ lib/PaySlips/payslips.dart
@@ -2,13 +2,16 @@ import 'package:App_idolconsulting/HomePage/homescrean.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
+
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+
 import 'dart:convert';
 
 //import '../homescrean.dart';
+
 import 'DetailsScreen.dart';
 import 'ListEmp.dart';
 import 'main.dart';
@@ -262,7 +265,7 @@ class MyApplState extends State<MyAppl> {
                                               final id = await FlutterDownloader
                                                   .enqueue(
                                                 url:
-                                                    "https://app.idolconsulting.co.za/idols/payslips/download/5ba3ac43c391b566c3c51e63",
+                                                    "https://app.idolconsulting.co.za/idols/payslips/download/5f7c38d0c391b506a26b74a1",
                                                 savedDir: externalDir.path,
                                                 fileName: "payslip",
                                                 showNotification: true,
